0483d1a32477ec389327c71b225cc1c017e5395a
trevisooriana <oritreviso@gmail.com>
2023-02-20 08:51:53 -0300

    inicio

---
+++ components.js
@@ -0,0 +1,21 @@
+(() => {
+    'use strict'
+  
+    const forms = document.querySelectorAll('.needs-validation')
+  
+
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+  function enviar_formulario(){
+    document.formulario1.submit()
+ }
