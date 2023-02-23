a75c773c7615fc706b9289109afb99d4a6b3184e
trevisooriana <oritreviso@gmail.com>
2023-02-23 10:37:53 -0300

    js probado

--- function.js
+++ function.js
@@ -1,9 +1,7 @@
-    document.getElementById('buttonenviar').onclick =function () 
-        
-     {
-         console.log("Se cargaron los datos correctamente");
+    document.getElementById('buttonenviar').onclick =(() => {
+        console.log("Se cargaron los datos correctamente");
         document.getElementById("demoButton").innerHTML = "Nos comunicaremos a la brevedad";
-    }
+    })
 
 
 
