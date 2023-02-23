af6b4483dd090fddf66620e323f5d5750257f501
trevisooriana <oritreviso@gmail.com>
2023-02-23 09:52:42 -0300

    js

---
+++ function.js
@@ -0,0 +1,10 @@
+    document.getElementById('buttonenviar').onclick =function () 
+        
+     {
+         console.log("Se cargaron los datos correctamente");
+        document.getElementById("demoButton").innerHTML = "Nos comunicaremos a la brevedad";
+    }
+
+
+
+   
